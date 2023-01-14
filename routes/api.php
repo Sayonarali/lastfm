f1651a33dac5f7199db7c020f95d628170d0d558
Sayonarali <elizaveta_shevchenko_00@inbox.ru>
2023-01-14 20:56:40 +0300

    dev: indent fixes

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::controller(AuthController::class)
-    ->group(function ()
-    {
-        Route::post('register', 'register');
-        Route::post('login', 'login');
-        Route::post('logout', 'logout');
-        Route::post('refresh', 'refresh');
-    });
+Route::controller(AuthController::class)->group(function ()
+{
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+});
 
